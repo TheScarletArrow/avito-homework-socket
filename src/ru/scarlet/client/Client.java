fcdfb8890c0f72c1877f920df84eee7e681b0356
Anton Yurkov <anton20001701@yandex.ru>
2022-04-10 18:47:16 +0300

    changes

--- src/ru/scarlet/client/Client.java
+++ src/ru/scarlet/client/Client.java
@@ -2,7 +2,8 @@ package ru.scarlet.client;
 
 import ru.scarlet.server.Server;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Scanner;
@@ -11,43 +12,35 @@ public class Client {
     public static void main(String[] args) {
 
 
-        try
-        {
-            Socket socket = new Socket("127.0.0.1", Server.SERVER_PORT); 		 //The IP address of the server and port number
-            System.out.println("Connection established");	   		 //Let the user know connection has been made
-            Scanner socketInput = new Scanner(socket.getInputStream()); //This is the input from the socket
+        try {
+            Socket socket = new Socket("127.0.0.1", Server.SERVER_PORT);
+            System.out.println("Connection established");
+            Scanner socketInput = new Scanner(socket.getInputStream()); //input from the socket
 
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true); //Output from the socket
-            Scanner in = new Scanner(System.in); 				 //Sets the keyboard as input for user
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true); //Output from the socket
+            Scanner in = new Scanner(System.in);
             System.out.println("Welcome to the game server!");
 
 
             String serverResp = "";
-            //Close the connection to server
-            while (true)
-                 {
-                String userInput = in.nextLine();        //Get input from the keyboard
-                printWriter.println(userInput);                    //Send user input to the socket
-                serverResp = socketInput.nextLine();     //Response from socket
-                System.out.println(serverResp);          //Print server response to screen
+            do {
+                String userInput = in.nextLine();
+                printWriter.println(userInput);
+                serverResp = socketInput.nextLine();
+                System.out.println(serverResp);
 
-                     if(serverResp.equals("Thank you for playing."))
-                         break;
-            }
-            System.out.println("Thank you for playing.");
+            } while (!serverResp.equals("Thank you for playing."));
 
-            //Close all connections and sockets to server
+
             printWriter.close();
             in.close();
             socketInput.close();
             socket.close();
-        }
-        catch (ConnectException e) //Will throw error if there is no connection to the server
+        } catch (ConnectException e)
         {
             System.err.println("Could not connect. Ensure server is running.");
             System.exit(-1);
-        }
-        catch (IOException e) 	   //Will throw error if there is no input
+        } catch (IOException e)
         {
             System.err.println("Couldn't get I/O for connection");
             System.exit(-1);
